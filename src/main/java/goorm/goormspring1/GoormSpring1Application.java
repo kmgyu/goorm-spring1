dc93d801ccab34240d722b9d8c482fc0eb82fd1f
kmgyu <lidersy961@gmail.com>
2025-08-29 11:23:33 +0900

    git init

---
+++ src/main/java/goorm/goormspring1/GoormSpring1Application.java
@@ -0,0 +1,13 @@
+package goorm.goormspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormSpring1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormSpring1Application.class, args);
+    }
+
+}
---
+++ src/test/java/goorm/goormspring1/GoormSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.goormspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormSpring1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
