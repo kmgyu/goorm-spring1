ebeac25b008e2b8ed40313f5fad011bc1a9f29ea
kmgyu <lidersy961@gmail.com>
2025-09-03 15:30:53 +0900

    refactor : split validation message

--- src/main/java/goorm/goormspring1/post/Post.java
+++ src/main/java/goorm/goormspring1/post/Post.java
@@ -20,18 +20,18 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // 자동 증가
     private Long seq;  // 게시글 번호
 
-    @NotBlank(message = "제목은 필수입니다")
-    @Size(min = 1, max = 200, message = "제목은 1~200자로 입력해주세요")
+    @NotBlank(message = "{post.title.notblank}")
+    @Size(min = 1, max = 200, message = "{post.title.size}")
     @Column(nullable = false, length = 200)
     private String title;  // 제목
 
-    @NotBlank(message = "내용은 필수입니다")
-    @Size(min = 5, message = "내용은 5자 이상 작성해주세요")
+    @NotBlank(message = "{post.content.notblank}")
+    @Size(min = 5, message = "{post.content.size}")
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;  // 내용
 
-    @NotBlank(message = "작성자는 필수입니다")
-    @Size(min = 1, max = 200, message = "작성자는 2~50자로 입력해주세요")
+    @NotBlank(message = "{post.author.notblank}")
+    @Size(min = 2, max = 200, message = "{post.author.size}")
     @Column(nullable = false, length = 50)
     private String author;  // 작성자
 
