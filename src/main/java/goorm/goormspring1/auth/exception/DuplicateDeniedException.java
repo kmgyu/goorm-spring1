491f17f4e9ed51daff23e7015f12563f379fadd7
kmgyu <lidersy961@gmail.com>
2025-09-09 13:51:17 +0900

    chore : add messages and remove unused classes

--- src/main/java/goorm/goormspring1/auth/exception/DuplicateDeniedException.java
+++
@@ -1,7 +0,0 @@
-package goorm.goormspring1.auth.exception;
-
-public class DuplicateDeniedException extends RuntimeException {
-    public DuplicateDeniedException(String message) {
-        super( "Email Duplicate Denied" + message);
-    }
-}
